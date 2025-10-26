6bb583f1c748ff23a634fc7547f39415ea41351c
sachinparihar <sachinparihar98120@gmail.com>
2025-10-26 19:58:09 +0530

    Add Array Solution

---
+++ src/MaxProfit.java
@@ -0,0 +1,22 @@
+public class MaxProfit {
+
+    public static int maxProfit(int[] prices) {
+        int min = Integer.MAX_VALUE;
+        int profit = 0;
+        for (int i = 0; i < prices.length; i++) {
+          if(prices[i] < min){
+              min = prices[i];
+          }
+          if(prices[i] - min > profit){
+              profit = prices[i] - min;
+          }
+        }
+        return profit;
+    }
+
+
+    public static void main(String[] args) {
+        int[] prices = {7,1,5,3,6,9};
+        System.out.println(maxProfit(prices));
+    }
+}
---
+++ src/Sum.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class Sum {
+
+    public static int[] twoSum(int[] nums, int target) {
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            Integer ComplementsIndex = map.get(nums[i]);
+            if(ComplementsIndex != null){
+                return new int[]{ComplementsIndex, i};
+            }else{
+                map.put(target - nums[i], i );
+            }
+
+        }
+        return nums;
+    }
+
+
+    public static void main(String[] args) {
+        int[] ans = {5,2,4};
+        int target = 6;
+        int[] result = twoSum(ans,target);
+        System.out.println(Arrays.toString(result));
+    }
+}
