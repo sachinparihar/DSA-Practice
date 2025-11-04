b92e62776b2c0e2a22b37ddbbe3416e64d4d84b1
sachinparihar <sachinparihar98120@gmail.com>
2025-11-04 12:50:09 +0530

    practice anagram  question

---
+++ src/Strings/ValidAnagram.java
@@ -0,0 +1,35 @@
+package Strings;
+
+public class ValidAnagram {
+
+
+
+    public static boolean isAnagram(String s, String t ){
+
+
+        if(s.length() != t.length()){
+            return false;
+        }
+
+        int[] count = new int[26];
+
+        for (int i = 0; i < s.length(); i++) {
+            count[s.charAt(i) - 'a']++;
+        }
+
+        for (int i = 0; i < t.length(); i++) {
+            count[t.charAt(i) - 'a']--;
+            if(count[t.charAt(i) - 'a']<0){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+        String s = "anagram";
+        String t = "nagaram";
+        System.out.println(isAnagram(s,t));
+    }
+}
